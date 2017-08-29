7d6154cbde305217667481c36d439a3c2b7ad75b
Philip Loche <ploche@physik.fu-berlin.de>
2017-08-29 13:23:38 +0200

    revert changes in saxs to initial status

--- sfactor/sfactor.pyx
+++ sfactor/sfactor.pyx
@@ -1,135 +1,53 @@
-#cython: boundscheck=False
-#cython: wraparound=False
-#cython: cdivision=True
-
-#Import python packages
 import numpy as np
-#Import C derivates for speed up
+
 cimport numpy as np
 cimport cython
 from libc cimport math
 from cython.parallel cimport prange
 
-cdef float[:] CMFP_H = np.array((0.493,0.323,0.14,0.041,10.511,26.126,3.142,57.8,0.003),dtype=np.float32);
+@cython.boundscheck(False)
+@cython.wraparound(False)
 
-cdef inline tuple maxmin(a,b):
-  if (a > b):
-    return a,b
-  else:
-    return b,a
+cpdef tuple compute_structure_factor(double[:,:] positions, double[:] boxdimensions, double start_q, double end_q):
+    """Calculates S(|q|) for all possible q values. Returns the q values as well as the scattering factor."""
 
-cdef inline int Trag_noeq(int row, int col, int N) nogil:
-  """Returns the index of a 1D array representation from a upper triangular
-  matrix without diagonal with size N. Taken from:
-  http://www.codeguru.com/cpp/cpp/algorithms/general/article.php/c11211/TIP-Half-Size-Triangular-Matrix.htm"""
+    assert(boxdimensions.shape[0]==3);
+    assert(positions.shape[1]==3);
 
-  return row*(N-1) - (row-1)*((row-1) + 1)/2 + col - row - 1;
+    cdef Py_ssize_t i, j, k, l, n_atoms;
+    cdef int[:] maxn = np.empty(3,dtype=np.int32);
+    cdef double qx, qy, qz, qrr, qdotr, sin, cos;
+    cdef double[:] q_factor = np.empty(3,dtype=np.double);
 
-cdef void distance_matrix( float[:,:] positions, int n_atoms,
-                                  float[:] boxdimensions,
-                                  float[:] dist_mat, int nt) nogil:
-  """Calculates the upper triangle of the Euclidean distance matrix
-  using the minimum image convention for rectengular box."""
+    n_atoms = positions.shape[0];
+    for i in range(3):
+        q_factor[i] = 2*np.pi/boxdimensions[i];
+        maxn[i] = <int>math.ceil(end_q/<float>q_factor[i]);
 
-  cdef int i, j;
-  cdef float xr, yr, zr;
+    cdef double[:,:,:] S_array = np.zeros(maxn, dtype=np.double);
+    cdef double[:,:,:] q_array = np.zeros(maxn, dtype=np.double);
 
-  for i in prange(n_atoms, schedule="dynamic", num_threads=nt):
+    for i in prange(<int>maxn[0],nogil=True):
+        qx = i * q_factor[0];
 
-    for j in range(i+1,n_atoms):
-        xr = positions[i,0] - positions[j,0];
-        yr = positions[i,1] - positions[j,1];
-        zr = positions[i,2] - positions[j,2];
+        for j in range(maxn[1]):
+            qy = j * q_factor[1];
 
-        # minimum image
-        xr = xr - boxdimensions[0]*math.round(xr/boxdimensions[0]);
-        yr = yr - boxdimensions[1]*math.round(yr/boxdimensions[1]);
-        zr = zr - boxdimensions[2]*math.round(zr/boxdimensions[2]);
+            for k in range(maxn[2]):
+                if (i + j + k != 0):
+                    qz = k * q_factor[2];
+                    qrr = math.sqrt(qx*qx+qy*qy+qz*qz);
 
-        dist_mat[Trag_noeq(i, j, n_atoms)] = math.sqrt(xr*xr+yr*yr+zr*zr);
+                    if (qrr >= start_q and qrr <= end_q):
+                        q_array[i,j,k] = qrr;
 
-cdef float CMSF(float q, int nh, float[:] CMFP) nogil:
-  """Calculates the form factor for the given Cromer-Mann scattering parameters."""
+                        sin = 0;
+                        cos = 0;
+                        for l in range(n_atoms):
+                            qdotr = positions[l,0]*qx + positions[l,1]*qy + positions[l,2]*qz;
+                            sin += math.sin(qdotr);
+                            cos += math.cos(qdotr);
 
-  cdef float q2, form_factor;
-  cdef int i;
+                        S_array[i,j,k] += sin*sin + cos*cos;
 
-  if nh > 0:
-      form_factor = CMSF(q,0,CMFP) + nh*CMSF(q,0,CMFP_H)
-  else:
-      form_factor = CMFP[8]
-      q2 = q*q/(8*math.pi*math.pi*10*10) # factor of 10 to convert from 1/nm to 1/Angstroms
-      for i in range(4):
-          form_factor =  form_factor + CMFP[i] * math.exp(-1*CMFP[i+4]*q2)
-
-  return form_factor;
-
-cdef double debye(float q, int[:] indices, float[:] dist_mat,
-                          int n_atoms, int[:] nh,
-                          float[:,:] CMFP, float[:] form_factors,
-                          float r_min, float volume ) nogil:
-  """Calculates the scattering intensity according to the Debye formula
-  for the given Cromer-Mann scattering parameters."""
-
-  cdef double r, qc, norm = 0, scat_int = 0
-  cdef int i, j
-
-  #calculate form factors for given q
-  for i in range(form_factors.shape[0]):
-    form_factors[i] = CMSF(q, nh[i], CMFP[i,:])
-
-  #Debye eq: sum_i f[i]*f[i] + sum_i sum_{j>i} 2 *f _i * f_j * sin(q * r_ij) / ( q * r_ij )
-  for i in range(n_atoms):
-    scat_int = scat_int + form_factors[indices[i]] * form_factors[indices[i]];
-    for j in range(i+1,n_atoms):
-        r = dist_mat[Trag_noeq(i, j, n_atoms)];
-        if r > r_min: continue
-        scat_int = scat_int + 2 * math.sin( q * r ) / ( q * r ) \
-                             * form_factors[indices[i]] * form_factors[indices[j]];
-
-  #normalization: sum_i f_i**2
-  for i in range(n_atoms):
-    norm = norm + form_factors[indices[i]]*form_factors[indices[i]]
-
-  scat_int = scat_int/norm
-
-  #cut-off correction
-  qc = q*r_min
-  scat_int = scat_int + 4 * math.pi * n_atoms / ( volume * q * q) * ( \
-                                   r_min * math.cos(qc) - math.sin(qc) / q);
-
-  return scat_int
-
-cpdef double[:] compute_scattering_intensity(float[:,:] positions, int n_atoms,
-                        int[:] indices, float [:,:] CMFP, int[:] nh,
-                        float[:] boxdimensions, float[:] dist_mat,
-                        float [:] form_factors,
-                        float start_q, float dq, int nbins,
-                        int nt):
-  """Calculates S(|q|) for all possible q values. Returns the q values as well as the scattering factor."""
-
-  assert(boxdimensions.shape[0]==3);
-  assert(positions.shape[0]==n_atoms);
-  assert(positions.shape[1]==3);
-
-  cdef int i;
-  cdef float r_max, r_min, volume, q;
-
-  volume = boxdimensions[0]*boxdimensions[1]*boxdimensions[2]
-  r_max,r_min = maxmin(boxdimensions[0], boxdimensions[1]);
-  r_max = maxmin(boxdimensions[2], r_max)[0] / 2.0;
-  r_min = maxmin(boxdimensions[2], r_min)[1] / 2.0;
-
-  ref_q = math.pi / r_max;
-
-  cdef double[:] S_array  = np.zeros(nbins, dtype=np.double);
-
-  distance_matrix(positions, n_atoms, boxdimensions, dist_mat, nt);
-
-  for i in prange(nbins, nogil=True, schedule="dynamic", num_threads=nt):
-    q = start_q + (i + 0.5) * dq
-    if q > ref_q:
-        S_array[i] = debye(q, indices, dist_mat, n_atoms, nh, \
-                            CMFP, form_factors, r_min, volume);
-
-  return S_array;
+    return (q_array,S_array);
