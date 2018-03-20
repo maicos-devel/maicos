53d919bd786f6ab187ac698c1bdde4cb90e4b3d5
Philip Loche <ploche@physik.fu-berlin.de>
2018-03-20 15:32:44 +0100

    Generalized entry script

--- mdtools/__main__.py
+++ mdtools/__main__.py
@@ -4,34 +4,32 @@
 import argparse
 import sys
 
-build_apps = ["carbonstructure", "insert"]
+# Dictionary containing the app name and the directory
+apps = {"carbonstructure": "build", "insert": "build", "debyer": "ana",
+        "density": "ana", "diporder": "ana", "epsilon_bulk": "ana",
+        "epsilon_cylinder": "ana", "epsilon_planar": "ana", "saxs": "ana"}
 
-ana_apps = ["debyer", "density", "diporder", "epsilon_bulk", "epsilon_cylinder",
-            "epsilon_planar", "saxs"]
-
-apps = build_apps + ana_apps
-apps.sort()
+applist = list(apps.keys())
+applist.sort()
 
 parser = argparse.ArgumentParser(description="""
     A collection of scripts to analyse and build systems for molecular dynamics simulations.""",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("program", type=str, help="Program to start",
-                    choices=apps)
+                    choices=applist)
 
 
 def main():
 
     try:
-        if sys.argv[1] in build_apps:
-            exec("import build.{}".format(sys.argv[1]))
-            eval("build.{}.main()".format(sys.argv[1]))
-        elif sys.argv[1] in ana_apps:
-            exec("import ana.{}".format(sys.argv[1]))
-            eval("ana.{}.main()".format(sys.argv[1]))
+        if sys.argv[1] in applist:
+            exec("import {}.{} as app".format(apps[sys.argv[1]], sys.argv[1]))
         else:
             parser.parse_args()
     except IndexError:
         parser.parse_args()
+
+    app.main()
 
 
 if __name__ == "__main__":
