7f0967248c093f90d15576a2020ad1e66741a8be
Philip Loche <ploche@physik.fu-berlin.de>
2018-12-14 17:10:02 +0100

    Faster CLI startup
    Simplified module loading to speed up cli interface.

--- mdtools/ana/__init__.py
+++ mdtools/ana/__init__.py
@@ -2,8 +2,12 @@
 # coding: utf-8
 
 __all__ = [
-    'density',
-    'epsilon',
-    'structure_analysis',
-    'timeseries',
+    'density.density_planar',
+    'epsilon.epsilon_bulk',
+    'epsilon.epsilon_planar',
+    'epsilon.dielectric_spectrum',
+    'structure_analysis.saxs',
+    'structure_analysis.debye',
+    'timeseries.dipole_angle',
+    'timeseries.kinetic_energy',
 ]
