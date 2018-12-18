b7ad764d2be53b3e8761e5053b048cee0db4eb13
Philip Loche <ploche@physik.fu-berlin.de>
2018-12-18 14:55:55 +0100

    Removed old dependency

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         package_data={'': ['share/*']},
         include_package_data=True,
         install_requires=[
-            'GromacsWrapper>=0.7', 'MDAnalysis>=0.19.0', 'matplotlib>=2.0.0',
+            'MDAnalysis>=0.19.0', 'matplotlib>=2.0.0',
             'numba>=0.38.0', 'numpy>=1.10.4', 'scipy>=0.17'
         ],
         entry_points={
