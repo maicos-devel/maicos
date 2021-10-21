89c14b1a2e3f52e8b27067798dd9ca2755835f22
Philip Loche <ploche@physik.fu-berlin.de>
2021-10-21 10:45:07 +0200

    Finished tests for decorator

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -8,12 +8,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import argparse
+from re import M
 
 import numpy as np
 import MDAnalysis as mda
 import pytest
 
-from numpy.testing import assert_equal, assert_almost_equal
+from numpy.testing import assert_equal, assert_allclose
 
 from maicos.modules.base import SingleGroupAnalysisBase, MultiGroupAnalysisBase
 from maicos.decorators import planar_base, charge_neutral
@@ -148,7 +149,7 @@ class TestPlanarBase(object):
 
     def test_orig_calculate_results(self, planar_class_obj):
         planar_class_obj._prepare()
-        planar_class_obj._frame_index = 1
+        planar_class_obj._frame_index = 0
         planar_class_obj._calculate_results()
 
         assert planar_class_obj.calculated_results
@@ -161,9 +162,7 @@ class TestPlanarBase(object):
         planar_class_obj._ts = planar_class_obj._universe
 
         # Universe dimensions are [2 nm, 2 nm, 6 nm]
-
         positions = np.array([[.1, .1, .1], [.6, 1.2, 2.2]]) # in nm
-
         assert_equal(planar_class_obj.get_bins(10 * positions, dim), bins)
 
     @pytest.mark.parametrize('dim', (0, 1, 2))
@@ -171,17 +170,94 @@ class TestPlanarBase(object):
         planar_class_obj = PlanarClass(ag, pos_arg=42, dim=dim)
         assert planar_class_obj.dim == dim
 
-    def test_binwidth(self, planar_class_obj):
-        pass
+    def test_binwidth(self, ag):
+        binwidth = 0.2
+        planar_class_obj = PlanarClass(ag, pos_arg=42, binwidth=binwidth)
+        planar_class_obj._prepare()
 
-    def test_center(self, planar_class_obj):
-        pass
+        assert planar_class_obj.binwidth == binwidth
+        assert planar_class_obj.n_bins == 60/(binwidth*10)
 
-    def test_comgroup(self, planar_class_obj):
-        pass
+    def test_n_bins(self, planar_class_obj, capsys):
+        planar_class_obj._verbose = True
+        planar_class_obj._prepare()
 
-    def test_comgroup_verbose(self, planar_class_obj):
-        pass
+        assert planar_class_obj.n_bins == 60
+
+        captured = capsys.readouterr()
+        assert "Using 60 bins." in captured.out
+
+    def test_zmin(self, ag):
+        binwidth = 0.2
+        planar_class_obj = PlanarClass(ag, pos_arg=42, binwidth=binwidth)
+        planar_class_obj.zmin = 1
+        planar_class_obj._prepare()
+
+        assert planar_class_obj.n_bins == (60 - 10)/(binwidth*10)
+
+
+    def test_zmax(self, ag):
+        binwidth = 0.2
+        planar_class_obj = PlanarClass(ag, pos_arg=42, binwidth=binwidth)
+        planar_class_obj.zmax = 5
+        planar_class_obj._prepare()
+
+        assert planar_class_obj.n_bins == 50/(binwidth*10)
+
+    def test_zmin_zmax(self, ag):
+        binwidth = 0.2
+        planar_class_obj = PlanarClass(ag, pos_arg=42, binwidth=binwidth)
+        planar_class_obj.zmin = 1
+        planar_class_obj.zmax = 5
+        planar_class_obj._prepare()
+
+        assert planar_class_obj.n_bins == (50 - 10)/(binwidth*10)
+
+    def test_results_z(self, ag):
+        planar_class_obj = PlanarClass(ag, pos_arg=42, center=False)
+        planar_class_obj._prepare()
+        planar_class_obj.Lz = 60
+        planar_class_obj._frame_index = 0
+        planar_class_obj._calculate_results()
+
+        assert_allclose(planar_class_obj.results["z"],
+                            np.linspace(0.05, 6 - 0.05, 60, endpoint=False))
+
+    def test_center(self, ag):
+        planar_class_obj = PlanarClass(ag, pos_arg=42, center=True)
+        planar_class_obj._prepare()
+        planar_class_obj._frame_index = 0
+        planar_class_obj.Lz = 60
+        planar_class_obj._calculate_results()
+
+        assert (planar_class_obj.results["z"]).min() == -3 + 0.05
+
+    def test_comgroup(self, ag):
+        planar_class_obj = PlanarClass(ag, pos_arg=42, comgroup="name OW")
+        planar_class_obj._prepare()
+
+        assert planar_class_obj.center
+
+        planar_class_obj._ts = planar_class_obj._universe
+        planar_class_obj._single_frame()
+
+        assert_allclose(ag.atoms.positions[0,:], 
+                            [33.88, 23.01, 37.62], rtol=1e-01)
+
+    def test_comgroup_no_atoms(self, ag):
+        planar_class_obj = PlanarClass(ag, pos_arg=42, comgroup="name foo")
+        with pytest.raises(ValueError, match="does not contain any atoms"):
+            planar_class_obj._prepare()
+
+    def test_comgroup_verbose(self, ag, capsys):
+        planar_class_obj = PlanarClass(ag,
+                                       pos_arg=42, 
+                                       comgroup="name OW")
+        planar_class_obj._verbose = True
+        planar_class_obj._prepare()
+        captured = capsys.readouterr()
+        assert "Performing the binning relative" in captured.out
+
 
 
 class TestChargedDecorator(object):
