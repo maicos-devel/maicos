f67bca816b2f80ea4c1bd253bc9abfcc6c56f202
hejamu <info@jaegerhenrik.de>
2022-10-10 16:36:05 +0200

    Release 0.6.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import Extension, setup
 
 # NOTE: keep in sync with __version__ in maicos.__init__.py
 # NOTE: keep in sync with version in docs/source/conf.py
-VERSION = "0.6"
+VERSION = "0.6.1"
 is_release = 'dev' not in VERSION
 
 
