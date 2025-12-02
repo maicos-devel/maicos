ccc6874b5e0a7a025e0963e8e842a8bfced026d4
Kira Fischer <kira.fischer@tuhh.de>
2025-12-02 15:25:58 +0100

    testing that serial and dask backend are the same

--- test_parallel.py
+++ test_parallel.py
@@ -6,6 +6,7 @@
 
 import MDAnalysis as mda
 import maicos
+import numpy as np
 
 mda.start_logging()
 
@@ -14,8 +15,8 @@ if __name__ == "__main__":
     u = mda.Universe("run.tpr", "run.xtc", in_memory=True)
     len(u.trajectory)
 
-    dens = maicos.DensityPlanar(u.atoms, jitter=0.0)
-    dens.run(verbose=True, step=10, backend="serial", n_workers=1)
+    #dens = maicos.DensityPlanar(u.atoms, jitter=0.0)
+    #dens.run(verbose=True, step=10, backend="serial", n_workers=1)
     #dens_jitter = maicos.DensityPlanar(u.atoms, bin_width=1e-6, jitter=0.01).run(step=10, verbose=True)
 
 
@@ -25,12 +26,21 @@ if __name__ == "__main__":
 
     ana_collection = maicos.core.AnalysisCollection(*[ana_obj])
 
-    #ana_collection.run(verbose=False, n_workers=2, backend='dask', step=10)
+    ana_collection.run(verbose=False, n_workers=4, backend='dask', step=10)
+
+    means = np.copy(ana_obj.results.eps_perp)
+    stds = np.copy(ana_obj.results.deps_perp)
+
     ana_collection.run(verbose=True, n_workers=1, backend='serial', step=10)
 
-    print(ana_obj.frames)
-    print(ana_obj._obs)
-    print(ana_obj.means)
+    print("asserting that they are the same")
+    np.testing.assert_allclose(means, ana_obj.results.eps_perp)
+    np.testing.assert_allclose(stds, ana_obj.results.deps_perp)
+    print("assertions complete")
+    
+    #print(ana_obj.frames)
+    #print(ana_obj._obs)
+    #print(ana_obj.means)
 
     #print(ana_obj._obs)
     #print(ana_obj.results.eps_perp)
