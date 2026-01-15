6cceccb651dbbb6f88af4e86fb09ade925dcb9c8
Kira Fischer <kira.fischer@tuhh.de>
2026-01-15 16:23:10 +0100

    first implementation of the z_cutting for radial component

--- src/maicos/modules/dielectriccylinder.py
+++ src/maicos/modules/dielectriccylinder.py
@@ -153,6 +153,9 @@ class DielectricCylinder(CylinderBase):
     def _single_frame(self) -> float:
         super()._single_frame()
 
+        # Calculate radial component by integrating over the charge
+        #==========================================================
+
         # Precalculate the bins each atom belongs to.
         rbins = np.digitize(self.pos_cyl[self.atomgroup.ix, 0], self._obs.bin_edges[1:-1])
 
@@ -173,21 +176,25 @@ class DielectricCylinder(CylinderBase):
         # q(a) = 2π * L * int_0^a ρ(r) * r dr,
         # where q(a) is the charge enclosed within a cylinder of radius a and length L.
         # This allows us to avoid numerical errors.
-        self._obs.m_r = -np.cumsum(curQ_r, axis=-1) / 2 / np.pi / self._obs.L_zbin / self._obs.bin_pos
+        self._obs.m_r = -np.cumsum(curQ_r, axis=-1) / 2 / np.pi / self._obs.L_zbin / self._obs.bin_pos # might have to expand dims of bin_pos
 
+
+        # NOTE: the z_binning only works, if there is nothing but water in the system :)
+        """
         # Same as above, but for the whole system.
         curQ_r_tot = np.bincount(
             rbins, weights=self._universe.atoms.charges, minlength=self.n_bins
         )
+        """
 
         self._obs.m_r_tot = (
-            -np.cumsum(curQ_r_tot) / 2 / np.pi / self._obs.L_zbin / self._obs.bin_pos
+            -np.cumsum(curQ_r, axis=-1) / 2 / np.pi / self._obs.L_zbin / self._obs.bin_pos
         )
 
         # Note that M_r is not really the total system dipole moment in radial
         # direction, but it keeps the nomenclature consistent across all of the
         # dielectric modules.
-        self._obs.M_r = np.sum(self._obs.m_r_tot * self._obs.bin_width)
+        self._obs.M_r = np.sum(self._obs.m_r_tot * self._obs.bin_width, axis=-1)
         self._obs.mM_r = self._obs.m_r * self._obs.M_r
 
         # Use virtual cutting method (for axial component)
@@ -418,13 +425,15 @@ class DielectricCylinder(CylinderBase):
         self.results.eps_z = self._pref * cov_z
         self.results.deps_z = self._pref * dcov_z
 
-        self.results.m_r = self.means.m_r
-        self.results.dm_r = self.sems.m_r
-        self.results.eps_r = -(
-            2 * np.pi * self.means.L * self._pref * self.results.bin_pos * cov_r
+        self.results.m_r = np.mean(self.means.m_r, axis=0)
+        self.results.dm_r = np.mean(self.sems.m_r, axis=0)
+        self.results.eps_r = -np.mean(
+            2 * np.pi * self.means.L * self._pref * self.results.bin_pos * cov_r,
+            axis=0
         )
-        self.results.deps_r = (
-            2 * np.pi * self.means.L * self._pref * self.results.bin_pos * dcov_r
+        self.results.deps_r = np.mean(
+            2 * np.pi * self.means.L * self._pref * self.results.bin_pos * dcov_r,
+            axis=0
         )
 
         self.results.m_phi = self.means.m_phi
