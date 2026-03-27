686084a57f673eec8e2d729196e49a4b63e9b309
hejamu <info@jaegerhenrik.de>
2026-03-27 09:04:57 +0100

    Add requested changes

--- src/maicos/core/base.py
+++ src/maicos/core/base.py
@@ -103,7 +103,7 @@ class _Runner:
             with logging_redirect_tqdm():
                 for analysis_object in analysis_instances:
                     ts.positions[:] = ts_original.positions
-                    ts.dimensions = ts_original.dimensions
+                    ts.dimensions[:] = ts_original.dimensions
                     if ts.has_velocities:
                         ts.velocities[:] = ts_original.velocities
                     if ts.has_forces:
