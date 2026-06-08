a5b9b83ccbbed447635f2653f3da50ba775cfafd
Quewakira <kira.fischer@tuhh.de>
2026-06-08 12:46:48 +0200

    make jitter test faster by reducing the univsere dimension to 2 angstrom

--- tests/core/test_base.py
+++ tests/core/test_base.py
@@ -999,6 +999,11 @@ class Test_AnalysisBase:
         density profile has no peak at a position of 100 (which would be the case
         without jitter).
         """
+        dims = ag_single_frame.universe.dimensions.copy()
+        dims[2] = 2
+        ag_single_frame.universe.dimensions = dims
+        ag_single_frame = ag_single_frame[ag_single_frame.positions[:, 2] <= 2]
+
         dens = DensityPlanar(ag_single_frame, bin_width=1e-6, jitter=0.0).run()
         dens_jitter = DensityPlanar(ag_single_frame, bin_width=1e-6, jitter=0.01).run()
 
